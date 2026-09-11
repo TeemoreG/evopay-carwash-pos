44489e0372ca278a416bc5681b455b0346f7b5af
Your Name <you@example.com>
2026-09-11 17:07:07 +0300

    Express 5: remove app.options('*'), rely on app.use(cors)

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'tin', 'bhfId', 'cmckey', 'Origin', 'Accept'],
 }));
 
-app.options('*', cors());
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
